df7f6fb6ecd3031c95eed6948fe0d1e7137908be
Fs Farhaan Shaikh <53146550+FSfarhaan@users.noreply.github.com>
2023-07-30 21:01:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const apiKey = "50977b5ae7d4544fa2fabd537b49c328";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-document.getElementById('btn').addEventListener("click", ()=>{
-    dispWeather(document.querySelector('.search input').value);
-});
+//Getting the search button;
+let btn = document.getElementById('btn');
 
+//On clicking the search button; the value of the input field should be stored in a variable and passed onto the function dispWeather() as a name of the city;
+btn.addEventListener('click', ()=>{
+    let inputVal = document.querySelector('.search input').value;
+    dispWeather(inputVal);
+})
 
+//Getting the details via API
 async function dispWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
     console.log(data);
 
+    //Displaying the temperature of the city
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "&deg;C";
+
+    //Displaying the name of the city
     document.querySelector(".city").innerHTML = data.name;
+
+    //Displaying the humidity of the city
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    
+    //Displaying the widn speed of the city
     document.querySelector(".wind").innerHTML = data.wind.speed + "kmPh";
 }
